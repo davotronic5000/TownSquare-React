a2905ef281474d53510852fd9e2e4d3b9c8d1c2d
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-02-25 22:21:03 +0000

    Playing around with a welcome message in townsquare

--- src/townsquare/Townsquare.tsx
+++ src/townsquare/Townsquare.tsx
@@ -1,3 +1,13 @@
+import {
+    Box,
+    CardHeader,
+    Card,
+    Heading,
+    CardBody,
+    Text,
+    ButtonGroup,
+    Button,
+} from "@chakra-ui/react";
 import { ComponentType, useState } from "react";
 
 enum TownsquareStatus {
@@ -7,8 +17,70 @@ enum TownsquareStatus {
 }
 
 const Townsquare: ComponentType = () => {
-    const townsquareStatus = useState<TownsquareStatus>(TownsquareStatus.Lost);
-    return <div>Townsquare</div>;
+    const [townsquareStatus, updateStatus] = useState<TownsquareStatus>(
+        TownsquareStatus.Lost
+    );
+    return (
+        <Box>
+            <Card>
+                <CardHeader>
+                    <Heading>Welcome to the Townsquare Tool</Heading>
+                </CardHeader>
+                <CardBody>
+                    {townsquareStatus === TownsquareStatus.Lost && (
+                        <Box>
+                            <Text>
+                                Would you like to host a session as a
+                                storyteller? Or are you a player?
+                            </Text>
+                            <ButtonGroup>
+                                <Button
+                                    onClick={() =>
+                                        updateStatus(
+                                            TownsquareStatus.Storyteller
+                                        )
+                                    }
+                                >
+                                    Storyteller
+                                </Button>
+                                <Button
+                                    onClick={() =>
+                                        updateStatus(TownsquareStatus.Player)
+                                    }
+                                >
+                                    Player
+                                </Button>
+                            </ButtonGroup>
+                        </Box>
+                    )}
+                    {townsquareStatus === TownsquareStatus.Storyteller && (
+                        <Box>
+                            <Text>Add some players to set up a game?</Text>
+                            <Button
+                                onClick={() =>
+                                    updateStatus(TownsquareStatus.Lost)
+                                }
+                            >
+                                Back
+                            </Button>
+                        </Box>
+                    )}
+                    {townsquareStatus === TownsquareStatus.Player && (
+                        <Box>
+                            <Text>Please select a seat! Have fun!</Text>
+                            <Button
+                                onClick={() =>
+                                    updateStatus(TownsquareStatus.Lost)
+                                }
+                            >
+                                Back
+                            </Button>
+                        </Box>
+                    )}
+                </CardBody>
+            </Card>
+        </Box>
+    );
 };
 
 export default Townsquare;
